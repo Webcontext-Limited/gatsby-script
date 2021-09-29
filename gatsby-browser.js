553658cb26db0a548f7a984eb8fbc73872d1460b
Verner Keel <verner.keel@gmail.com>
2021-09-29 22:13:17 +0300

    Set timeout to trustbox

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -15,7 +15,13 @@ const addContent = (content) => {
 };
 
 exports.onInitialClientRender = () => {
-  addScript('//widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js');
+  setTimeout(
+    () =>
+      addScript(
+        '//widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js'
+      ),
+    3000
+  );
   /*
   addScript('//cdn.dni.nimbata.com/288803921.min.js');
   addScript('https://mms.user.com/widget.js');
@@ -28,9 +34,13 @@ exports.onInitialClientRender = () => {
   );
   */
 
-  addContent(`<div class="trustpilot-widget" data-locale="en-GB" data-template-id="53aa8912dec7e10d38f59f36" data-businessunit-id="5200f51800006400057311fa" data-style-height="140px" data-style-width="100%" data-theme="light" data-stars="1,2,3,4,5" data-review-languages="en">
+  setTimeout(
+    () =>
+      addContent(`<div class="trustpilot-widget" data-locale="en-GB" data-template-id="53aa8912dec7e10d38f59f36" data-businessunit-id="5200f51800006400057311fa" data-style-height="140px" data-style-width="100%" data-theme="light" data-stars="1,2,3,4,5" data-review-languages="en">
   <a href="https://uk.trustpilot.com/review/marymonson.co.uk" target="_blank" rel="noopener">Trustpilot</a>
-</div>`);
+</div>`),
+    3000
+  );
 
   console.log('this is window.onload func');
 
