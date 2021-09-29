805a01a31fa58ef2867e2e2c58e0a6fed11c7c80
Verner Keel <verner.keel@gmail.com>
2021-09-29 20:45:04 +0300

    onInitialClientRender without window.onload

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,15 +2,16 @@ const addScript = (url) => {
   const script = document.createElement('script');
   script.type = 'text/javascript';
   script.src = url;
-  script.async = true;
+
   document.body.appendChild(script);
 };
 
 exports.onInitialClientRender = () => {
-  window.onload = () => {
-    addScript('//cdn.dni.nimbata.com/288803921.min.js');
-    addScript(
-      '//widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js'
-    );
-  };
+  addScript('//cdn.dni.nimbata.com/288803921.min.js');
+  addScript('//widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js');
+  console.log('this is window.onload func');
+
+  console.log('This is loaded by onInitialClientRender');
 };
+
+// window.onLoad=() =>
