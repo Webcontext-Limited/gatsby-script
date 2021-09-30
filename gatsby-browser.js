2699ec3e3321e0f03ad3ef3a581c839ba7b1cea2
Verner Keel <verner.keel@gmail.com>
2021-09-30 13:00:10 +0300

    Nimbata through onInitialClientRender window.load

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -20,7 +20,7 @@ const onLoad = (script) =>
   });
 
 exports.onInitialClientRender = () => {
-  addScript('cdn.dni.nimbata.com/288803921.min.js');
+  onLoad(addScript('cdn.dni.nimbata.com/288803921.min.js'));
 };
 
 //setTimeout(() => addScript('//cdn.dni.nimbata.com/288803921.min.js'), 2000);
